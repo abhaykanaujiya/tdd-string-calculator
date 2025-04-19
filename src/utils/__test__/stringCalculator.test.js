e27ab242533249fe33dca0759e397a0dc983aece
Abhay kanaujiya <abhaykanaujiya@gmail.com>
2025-04-19 19:14:53 +0000

    sixth test case added for negative numbers in stringCalculator utils

--- src/utils/__test__/stringCalculator.test.js
+++ src/utils/__test__/stringCalculator.test.js
@@ -20,5 +20,9 @@ describe('String Calculator', () => {
   test('supports custom delimiters', () => {
     expect(add('//;\n1;2')).toBe(3);
   });
+
+  test('throws exception for negative numbers', () => {
+    expect(() => add('-1,2,-3')).toThrow('negative numbers not allowed -1, -3');
+  });
  
 }); 
