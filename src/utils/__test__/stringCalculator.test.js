ac7214de3b7b9cbd00de3bc86f1148aaecc68833
Abhay kanaujiya <abhaykanaujiya@gmail.com>
2025-04-19 19:04:32 +0000

    fifth test case added for custom delimiters in stringCalculator utils

--- src/utils/__test__/stringCalculator.test.js
+++ src/utils/__test__/stringCalculator.test.js
@@ -16,5 +16,9 @@ describe('String Calculator', () => {
   test('handles new lines between numbers', () => {
     expect(add('1\n2,3')).toBe(6);
   });
+
+  test('supports custom delimiters', () => {
+    expect(add('//;\n1;2')).toBe(3);
+  });
  
 }); 
