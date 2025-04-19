8936a76e4625c66212483312d1da21e13035f1a3
Abhay kanaujiya <abhaykanaujiya@gmail.com>
2025-04-19 19:00:03 +0000

    fourth test case added for sum of new line between numbers in stringCalculator utils

--- src/utils/__test__/stringCalculator.test.js
+++ src/utils/__test__/stringCalculator.test.js
@@ -13,5 +13,8 @@ describe('String Calculator', () => {
     expect(add('1,5')).toBe(6);
   });
 
+  test('handles new lines between numbers', () => {
+    expect(add('1\n2,3')).toBe(6);
+  });
  
 }); 
